0b77c7fd408beecc1d35903982892fd89cdb9fd6
Jero <tokidokitalkyou@gmail.com>
2019-03-09 21:24:02 +0800

    Adding a tests for Daniel
    This project aims to adhere with the Test Driven Development approach.

---
+++ test/test_daniel.py
@@ -0,0 +1,40 @@
+import unittest
+import os
+import random
+import string
+from src.core import daniel
+
+
+class DanielTest(unittest.TestCase):
+
+    def test_create_email(self):
+
+        sender = 'tokidokitalkyou@gmail.com'
+        recipient = 'tokidokitalkyou@gmail.com'
+        subject = 'xxx'
+        message = 'xxxxxxxxxxxxx'
+
+        result = daniel.create_email(sender, recipient, subject, message)
+        self.assertIsInstance(result, type(result))
+
+    def test_save_email(self):
+
+        sender = 'tokidokitalkyou@gmail.com'
+        recipient = 'tokidokitalkyou@gmail.com'
+        subject = 'test save_email method'
+        message = 'is the email created?'
+
+        email = daniel.create_email(sender, recipient, subject, message)
+
+        suffix = ''.join([random.choice(string.digits) for index in range(4)])
+        filename = f'test_email{suffix}.eml'
+
+        daniel.save_email(email, filename)
+        check_filename = os.path.exists(filename)
+
+        test_message = f'\'{filename}\' not found in {os.getcwd()}'
+        self.assertTrue(check_filename, test_message)
+
+
+if __name__ == '__main__':
+    unittest.main()
